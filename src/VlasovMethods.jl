2082351b903030b81b9f5278e42bc897dbf8615b
Sandra <u7073543@anu.edu.au>
2024-06-26 20:34:54 +1000

    updates for Landau calculation; adds Picard solver for Landau

--- src/VlasovMethods.jl
+++ src/VlasovMethods.jl
@@ -19,13 +19,14 @@ using LinearAlgebra
 using QuadGK
 using HCubature
 using Plots
+using SimpleSolvers
 
 import GeometricEquations
 import GeometricEquations: ntime
-import GeometricIntegrators
-import DifferentialEquations
-import NaNMath
-
+import GeometricIntegrators.Integrators
+import GeometricIntegrators.Extrapolators
+# import DifferentialEquations
+# import NaNMath
 
 # abstract types
 
@@ -75,12 +76,14 @@ export projection
 include("methods/splitting.jl")
 include("methods/diffeq_integrator.jl")
 include("methods/geometric_integrator.jl")
+include("methods/Landau_solver.jl")
 
 export run!
 export run
 export SplittingMethod
 export DiffEqIntegrator
 export GeometricIntegrator
+export Picard_iterate_over_particles
 
 
 # Vlasov models
