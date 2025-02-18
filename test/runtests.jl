f7998abbd5b47a1e0905cc9cdef1b83ee935171a
Michael Kraus <michael.kraus@ipp.mpg.de>
2025-02-18 17:37:00 +0100

    Move spline basis tests to separate file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 
 @testset "VlasovMethods.jl" begin
     include("spline_tests.jl")
+    include("spline_basis_tests.jl")
     # include("projections_tests.jl")
     # include("electric_field_tests.jl")
 end
